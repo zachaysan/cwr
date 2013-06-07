656481c243507af4133b0254e1fac39044b8f2d4
Zach Aysan <zachaysan@gmail.com>
2013-06-07 16:04:51 -0400

    Stub out Webhook post

--- lib/cwr.rb
+++ lib/cwr.rb
@@ -1,5 +1,13 @@
 class CWR
-  def self.yearrr
-    puts "Avast ye matey!"
+  attr_accessor :access_token
+
+  def webhook
+    require_access_token
+  end
+
+  protected
+
+  def require_access_token
+    raise "access_token required for webhook" unless access_token
   end
 end
