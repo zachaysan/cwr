a75274e1fed4ab0798ecae8c507264b2c560f824
Zach Aysan <zachaysan@gmail.com>
2013-06-07 21:02:18 -0400

    Add register producer

--- lib/cwr.rb
+++ lib/cwr.rb
@@ -5,7 +5,7 @@ class CWR
   include HTTParty
   base_uri 'http://0.0.0.0:3000'
 
-  attr_accessor :access_token, :email, :password
+  attr_accessor :access_token, :email, :password, :producer
   alias_method :username, :email
   def username=(other)
     @email = other
@@ -18,6 +18,10 @@ class CWR
   end
 
   protected
+
+  def register_producer
+    raise "@producer required to register" unless @producer
+  end
 
   def require_access_token
     create_new_access_token_if_able unless access_token
